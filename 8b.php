9eca3fcb6889031d07d1bfee21830cf5e655c5ad
Adithya <adithya1489181@gmail.com>
2026-01-12 22:57:27 +0530

    one more update

--- 8b.php
+++ 8b.php
@@ -56,33 +56,28 @@
             // Fetch student records
             $stmt = $pdo->query("SELECT * FROM students");
             $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $n = count($students);
 
-            // Selection sort function
-            function selectionSort(&$arr, $n) {
-                for ($i = 0; $i < $n - 1; $i++) {
-                    $min_idx = $i;
-                    for ($j = $i + 1; $j < $n; $j++) {
-                        if ($arr[$j]['gpa'] < $arr[$min_idx]['gpa']) {
-                            $min_idx = $j;
-                        }
-                    }
-                    if ($min_idx != $i) {
-                        $temp = $arr[$i];
-                        $arr[$i] = $arr[$min_idx];
-                        $arr[$min_idx] = $temp;
+            for ($i = 0; $i < $n - 1; $i++) {
+                $min_idx = $i;
+                for ($j = $i + 1; $j < $n; $j++) {
+                    if ($students[$j]['gpa'] < $students[$min_idx]['gpa']) {
+                        $min_idx = $j;
                     }
                 }
+                if ($min_idx != $i) {
+                    $temp = $students[$i];
+                    $students[$i] = $arr[$min_idx];
+                    $arr[$min_idx] = $temp;
+                }
             }
-
-            // Sort students by GPA
-            selectionSort($students, count($students));
 
             // Display sorted student records
             echo "<table>";
             echo "<tr><th>ID</th><th>Name</th><th>GPA</th></tr>";
             foreach ($students as $student) {
                 echo "<tr>";
-                echo "<td>" . htmlspecialchars($student['id']) . "</td>";
+                echo "<td>{$student['id']}</td>";
                 echo "<td>" . htmlspecialchars($student['name']) . "</td>";
                 echo "<td>" . htmlspecialchars($student['gpa']) . "</td>";
                 echo "</tr>";
